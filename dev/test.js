8d4cda56618310d3ceed3357ff48ccaa3510555f
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-03-18 19:43:27 +0530

    Implemented Proof of Work

--- dev/test.js
+++ dev/test.js
@@ -2,25 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'IDIFUHUFHAIFA245FUFFDF1';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'ABCD',
-        recipient: 'EFGH'
-    },
-    {
-        amount: 50,
-        sender: 'XYZD',
-        recipient: 'UYHH'
-    },
-    {
-        amount: 45,
-        sender: 'KJJS',
-        recipient: 'POIU'
-    }
-];
-
-const nounce = 100;
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nounce));
+console.log(bitcoin);
